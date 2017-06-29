5b78302b540ab766bd851375e1ba614cd9401b6f
Roger A Villarreal <rogeravillarreal@gmail.com>
2017-06-29 10:44:03 -0500

    reset background to default

--- CalorieCalc/ViewController.swift
+++ CalorieCalc/ViewController.swift
@@ -21,27 +21,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setGradientBackground()
         
         tableView.delegate = self
         tableView.dataSource = self
         
         self.weightTextField.delegate = self
         self.poundsTextField.delegate = self
-        
-        
-    }
-    
-    func setGradientBackground() {
-        let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
-        let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
-        
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [ colorTop, colorBottom]
-        gradientLayer.locations = [ 0.0, 1.0]
-        gradientLayer.frame = self.view.bounds
-        
-        self.view.layer.addSublayer(gradientLayer)
+                
     }
     
     @IBAction func weightEditing(_ sender: Any) {
